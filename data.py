5f21c5082688301e283b1aa4a80ca2b51a67ab50
TThibeau <thibeau.teuwen@hotmail.com>
2022-04-02 00:15:03 +0200

    first commit

---
+++ data.py
@@ -0,0 +1,107 @@
+question_data = [
+    {
+        "response_code": 0,
+        "results": [
+            {
+                "category": "Entertainment: Music",
+                "type": "boolean",
+                "difficulty": "easy",
+                "question": "The music group Daft Punk got their name from a negative review they recieved.",
+                "correct_answer": "True",
+                "incorrect_answers": [
+                    "False"
+                ]
+            },
+            {
+                "category": "History",
+                "type": "boolean",
+                "difficulty": "easy",
+                "question": "The Cold War ended with Joseph Stalin&#039;s death.",
+                "correct_answer": "False",
+                "incorrect_answers": [
+                    "True"
+                ]
+            },
+            {
+                "category": "Animals",
+                "type": "boolean",
+                "difficulty": "easy",
+                "question": "Kangaroos keep food in their pouches next to their children.",
+                "correct_answer": "False",
+                "incorrect_answers": [
+                    "True"
+                ]
+            },
+            {
+                "category": "Science: Computers",
+                "type": "boolean",
+                "difficulty": "easy",
+                "question": "Pointers were not used in the original C programming language; they were added later on in C++.",
+                "correct_answer": "False",
+                "incorrect_answers": [
+                    "True"
+                ]
+            },
+            {
+                "category": "Entertainment: Video Games",
+                "type": "boolean",
+                "difficulty": "easy",
+                "question": "&quot;Half-Life 2&quot; runs on the Source Engine.",
+                "correct_answer": "True",
+                "incorrect_answers": [
+                    "False"
+                ]
+            },
+            {
+                "category": "General Knowledge",
+                "type": "boolean",
+                "difficulty": "easy",
+                "question": "A scientific study on peanuts in bars found traces of over 100 unique specimens of urine.",
+                "correct_answer": "False",
+                "incorrect_answers": [
+                    "True"
+                ]
+            },
+            {
+                "category": "Entertainment: Film",
+                "type": "boolean",
+                "difficulty": "easy",
+                "question": "In the original Star Wars trilogy, Alec Guinness provided the voice for Darth Vader.",
+                "correct_answer": "False",
+                "incorrect_answers": [
+                    "True"
+                ]
+            },
+            {
+                "category": "Entertainment: Film",
+                "type": "boolean",
+                "difficulty": "easy",
+                "question": "Brandon Routh plays the titular character in the movie &quot;John Wick&quot;.",
+                "correct_answer": "False",
+                "incorrect_answers": [
+                    "True"
+                ]
+            },
+            {
+                "category": "Geography",
+                "type": "boolean",
+                "difficulty": "easy",
+                "question": "Tokyo is the capital of Japan.",
+                "correct_answer": "True",
+                "incorrect_answers": [
+                    "False"
+                ]
+            },
+            {
+                "category": "Geography",
+                "type": "boolean",
+                "difficulty": "easy",
+                "question": "There is an island in Japan called \u014ckunoshima, A.K.A. &quot;Rabbit Island&quot;, so named because of it&#039;s huge population of rabbits.",
+                "correct_answer": "True",
+                "incorrect_answers": [
+                    "False"
+                ]
+            }
+        ]
+    }
+]
